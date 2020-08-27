da99ce650e715699e58bc6d4973bf7054023a9c4
DaehunKim <gidonskaris@gmail.com>
2020-08-27 17:38:36 +0900

    [Add] token verification user activation

--- router/user.js
+++ router/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
 const userModel = require("../model/user");
 
 
@@ -11,9 +13,54 @@ const checkAuth = passport.authenticate("jwt", {session : false});
 
 //회원가입 기능
 //@route POST http://localhost:3000/user/register
-//@desc  Register User
+//@desc  Register User / sendEmail for Activation
 //@access Public
 router.post("/register", register_user);
+
+//회원 가입한 계 활성화 기정
+//@route POST http://localhost:3000/user/activation
+//@desc  Activation account from confirm email
+//@access Private
+router.post("/activation", (req, res)=> {
+
+    const {token} = req.body; //이메일로 제공되는 토큰을 입력하는 것을 비구조할당 정  (물론 실제의 서비에선 이 토큰이 보이진 않겠지만)
+
+    if(token){
+        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, decoded) => {
+
+            if(err){
+                return res.status(400).json({
+                    error : "Expired Link. SignUp again please!" //토큰이 유효하지 않은거니까 기간이 만료된것일것.
+                })
+            }
+            else{
+
+                const {name, email, password} = jwt.decode(token);
+
+                const newUser = new userModel({
+                    name, email, password
+                });
+
+                console.log(jwt.decode(token));
+                console.log("userInfo", newUser);
+
+                newUser
+                    .save()
+                    .then(user => {
+                        return res.status(200).json({
+                            message : "successful signup",
+                            userInfo : user
+                        })
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            message : err.message
+                        })
+                    })
+            }
+        });
+    }
+});
 
 //로그인 기능
 //@route POST http://localhost:3000/user/login
