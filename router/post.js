8559d504545a7be2ae95b84fd3151b5d4f517ec6
DaehunKim <gidonskaris@gmail.com>
2020-08-28 20:26:15 +0900

    [Add] like/unlike post

--- router/post.js
+++ router/post.js
@@ -114,7 +114,7 @@ router.patch("/:post_id", checkAuth, (req, res) => {
         })
 });
 
-//게시글 업데이트
+//게시글 지우기
 //@route Delete http://localhost:3000/post/:post_id
 //@desc  Delete post
 //@access Private
@@ -139,5 +139,72 @@ router.delete("/:post_id", checkAuth, (req, res) => {
             }
         })
 });
+
+//게시글에 좋아요 누르기
+//routes Post http://localhost:3000/post/like/:post_id
+//desc   Like post
+//access Private
+router.post("/like/:post_id", checkAuth, (req, res) => {
+
+    const id = req.params.post_id;
+    //라이크를 한사람당 한명으로 유지하기 위해서, 유저의 라이크가 0보다 많으면 이미 좋아요 한거라고 리턴, 아니면 좋아요 등
+    postModel
+        .findById(id)
+        .then(post => {
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length >0){
+                return res.status(200).json({
+                    message : "user already liked this post"
+                })
+            }
+            else{
+                post.likes.unshift({user : req.user.id})
+                post
+                    .save()
+                    .then(post => {
+                        res.status(200).json(post)
+                    })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err.message
+            })
+        })
+});
+
+//게시글 좋아요 지우기
+//routes Delete http://localhost:3000/post/unlike/:post_id
+//desc   Delete like from post
+//access Private
+router.delete("/unlike/:post_id", checkAuth, (req, res )=> {
+
+    const id = req.params.post_id;
+    //먼저 좋아요들 중에서 해당 유저 아이디와 같은 아이디의 라이크가 없으면 좋아요 한적이 없다 리턴, 아닌 경우는 라이크중에 찾아서 없애주
+    postModel
+        .findById(id)
+        .then(post => {
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length ===0){
+                return res.status(400).json({
+                    message : "you've never liked this post"
+                })
+            }
+            const removeIndex = post.likes
+                .map(like => like.user.toString())
+                .indexOf(req.user.id)
+
+            post.likes.splice(removeIndex, 1)
+
+            post
+                .save()
+                .then(post => res.status(200).json(post))
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err.message
+            })
+        })
+});
+
+
 
 module.exports = router;
